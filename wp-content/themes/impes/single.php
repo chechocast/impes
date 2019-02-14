663305a22e9b8f2785f6e98de3f0cd1e1a4fb6f4
chechocast <chechocast@gmail.com>
2019-02-13 21:42:32 -0500

    termino de interna poryectos

--- wp-content/themes/impes/single.php
+++ wp-content/themes/impes/single.php
@@ -1,18 +1,29 @@
-<?php get_header(); ?>
+<?php get_header(); 
+  $defaultImageContent = get_the_post_thumbnail_url();
+  $videoUrlContent = get_field('link_de_video');
+  $hasImage = isset($defaultImageContent) && trim($defaultImageContent) !== '';
+  $hasVideo = isset($videoUrlContent) && trim($videoUrlContent) !== '';
+
+
+?>
 <div class="pg-body">
   <?php while (have_posts()): the_post(); ?>
     <section>
       <article class="content_all">
         <h1 class="tit-section"><?php the_title(); ?></h1>
         <div class="inter-txt">
-
+            <?php 
+            if($hasImage && !$hasVideo) { ?>
             <div class="img-post">
-              <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+              <img src="<?php echo $defaultImageContent; ?>" alt="">
             </div>
+            <?php } ?>
+            <?php if($hasVideo) { ?>
             <div class="video">
               <iframe src="<?php the_field('link_de_video'); ?>" frameborder="0" allowfullscreen></iframe>
               <img src="<?php echo get_template_directory_uri(); ?>/images/img-video.png">
             </div>
+            <?php } ?>
           <main>
             <?php the_content(); ?>
           </main>
